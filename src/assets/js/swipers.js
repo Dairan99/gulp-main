9994c42e17545ed6356d8f6fe8b978bb5f490061
Dairan99 <vadim.parshenkov@mail.ru>
2025-06-17 14:05:36 +0300

    summary 7.4

--- src/assets/js/swipers.js
+++ src/assets/js/swipers.js
@@ -41,7 +41,7 @@ const aboutClinickSwiper = new Swiper(".aboutClinickSwiper", {
 });
 
 const doctorsClinicSwiper = new Swiper(".doctorsSwiper", {
-    slidesPerView: 1.08,
+    slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
         nextEl: ".doctors-clinic__swiper-button-next",
@@ -68,8 +68,39 @@ const doctorsClinicSwiper = new Swiper(".doctorsSwiper", {
     },
 });
 
+const serviceSwiper = new Swiper(".serviceSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    grid: {
+        rows: 2,
+    },
+    navigation: {
+        nextEl: ".service__swiper-button-next",
+        prevEl: ".service__swiper-button-prev",
+    },
+    pagination: {
+        el: ".service__swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints: {
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 26,
+        },
+        850: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+    },
+});
+
 const modernNarcologySwiper = new Swiper(".modernNarcologySwiper", {
-    slidesPerView: 1.08,
+    slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
         nextEl: ".modern-narcology__swiper-button-next",
@@ -96,8 +127,65 @@ const modernNarcologySwiper = new Swiper(".modernNarcologySwiper", {
     },
 });
 
+
+const reviewsSwiper = new Swiper(".reviewsSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".reviews__swiper-button-next",
+        prevEl: ".reviews__swiper-button-prev",
+    },
+    pagination: {
+        el: ".reviews__swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints: {
+        1100: {
+            slidesPerView: 3,
+            spaceBetween: 26,
+        },
+        850: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        600: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+    },
+});
+
+const usefulInformationSwiper = new Swiper(".usefulInformationSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".useful-information__swiper-button-next",
+        prevEl: ".useful-information__swiper-button-prev",
+    },
+    pagination: {
+        el: ".useful-information__swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints: {
+        1100: {
+            slidesPerView: 4,
+            spaceBetween: 26,
+        },
+        850: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+    },
+});
+
 const licensySwiper = new Swiper(".licenseSwiper", {
-    slidesPerView: 1.08,
+    slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
         nextEl: ".license__swiper-button-next",
